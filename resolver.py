35d806092ac92553a27ef82bc9bc3f2eea78c3e5
NoamLeabo <noam.leabovich@gmail.com>
2024-11-19 11:48:48 +0200

    -Now it saves all the "next ones" as well
    need to change the "in" logic of searching NS only for a perfect match

--- resolver.py
+++ resolver.py
@@ -16,7 +16,7 @@ time_last_in_cache = float(x)
 # we create a socket for the resolver server
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # we bind it with its port
-s.bind(('', myPort))
+s.bind(('', 7979))
 # we initiate the cache
 cache = []
 
@@ -36,7 +36,7 @@ while True:
     # check if it's in the cache
     test = any(url in item[0] for item in cache)
 
-    # if url is in cache we send it back and we're done
+    # if url is in cache at full we send it back and we're done
     if test == True:
         # the url's index in cache
         index = next((i for i, item in enumerate(cache) if url in item[0]),
@@ -49,21 +49,38 @@ while True:
         data = addres
         # we send back the encoded data to the addr of the sender
         s.sendto(data.encode('utf-8'), addr)
+        continue
 
-    was_found = True
+    # we check if some of the url is in cache
+    was_found = False
     # we go over each line in the file
     for saved in cache:
         # if we have not found yet
         if not was_found:
             # we check for substring
-            if saved and saved.split(',')[0] in url:
+            if saved[0] and saved[0].split(',')[0] in url:
                 # we send the whole line that fit
-                next_one = saved.strip()
+                next_one = saved[0].strip()
                 # update the var accordingly
                 was_found = True
     
+    # if some of the url is in cache we need to find the rest of it
     if was_found:
+        # we create a socket to contact with the "father" (next_one in that case) 
+        f_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # we try to get the rest from the next_one server
         f_data = next_one
+        # split the f_data to extract the next IP and port
+        parts = f_data.split(',')
+        next_ip = parts[1].split(':')[0]
+        next_port = int(parts[1].split(':')[1])
+        # send the next request to the next server
+        f_socket.sendto(url.encode('utf-8'), (next_ip, next_port))
+        # wait for the next answer
+        next_data, next_addr = f_socket.recvfrom(1024)
+        # decode the next answered line
+        f_data = next_data.decode('utf-8')
+
         # if the line does not end with 'A' we play "Havila O've'ret" until we get the final result
         while f_data[-1] == 'S':
             # we store in the cache the data
@@ -79,11 +96,19 @@ while True:
             # decode the next answered line
             f_data = next_data.decode('utf-8')
         
-        # we add the new data for the cache with the curr time
-        cache.append((f_data, time.time()))
-        # we send the info back
-        s.sendto(f_data.encode('utf-8'), addr)
-        continue
+        # if we got a final answer
+        if f_data[-1] == 'A': 
+            # we add the new data for the cache with the curr time
+            cache.append((f_data, time.time()))
+            # we send the info back
+            s.sendto(f_data.encode('utf-8'), addr)
+            continue
+
+        # the final answer was that the result was not found no where    
+        else:
+            # we send the info back without store in cache
+            s.sendto(f_data.encode('utf-8'), addr)
+            continue
 
     # if url is not in cache we ask our father server for the req line
     else:
@@ -95,8 +120,8 @@ while True:
         f_data, f_addr = f_socket.recvfrom(1024)
         # decode the answered line
         f_data = f_data.decode('utf-8')
-        # if the line ends with 'A' it means we can send it back to the sender and we're done
 
+        # if the line ends with 'A' it means we can send it back to the sender and we're done
         if f_data[-1] == 'A':
             # we add the new data for the cache with the curr time
             cache.append((f_data, time.time()))
@@ -118,8 +143,17 @@ while True:
             next_data, next_addr = f_socket.recvfrom(1024)
             # decode the next answered line
             f_data = next_data.decode('utf-8')
+        
+        # if we got a final answer
+        if f_data[-1] == 'A': 
+            # we add the new data for the cache with the curr time
+            cache.append((f_data, time.time()))
+            # we send the info back
+            s.sendto(f_data.encode('utf-8'), addr)
+            continue
 
-        # we add the new data for the cache with the curr time
-        cache.append((f_data, time.time()))
-        # we send the info back
-        s.sendto(f_data.encode('utf-8'), addr)
+        # the final answer was that the result was not found no where    
+        else:
+            # we send the info back without store in cache
+            s.sendto(f_data.encode('utf-8'), addr)
+            continue
--- test2.py
+++ test2.py
@@ -5,11 +5,11 @@ zone_file = open('zone.txt', 'r').read()
 # we create the server's socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # we bind it with a port
-s.bind(('', 777))
+s.bind(('', 888))
 
 # the server loop
 while True:
     # receive a req
     url, addr = s.recvfrom(1024)
-    to_send = "w.co.il,127.0.0.1:888,NS"
+    to_send = "dont have :)"
     s.sendto(to_send.encode('utf-8'), addr)
